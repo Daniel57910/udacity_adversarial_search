681716d6d22f6b26672f87a83c67661b321a3011
Daniel Miller <daniel.miller@sainsburys.co.uk>
2020-08-18 12:13:44 +0100

    diags obtained: moving on

--- player.py
+++ player.py
@@ -7,9 +7,25 @@ class Player:
         self.loc["X"] = x
         self.loc["Y"] = y
 
-    def actions(self, board_size: int) -> dict:
+    def actions_rows(self, board_size: int) -> dict:
         actions = {}
         x, y = self.loc["X"], self.loc["Y"]
         rows = [(x, i) for i in range(0, board_size)]
         cols = [(i, y) for i in range(0, board_size)]
-        return set(rows + cols)
+        diags = self._diags(board_size)
+        return set(rows + cols + diags)
+
+    def _diags(self, board_size: int) -> dict:
+        x, y = self.loc["X"], self.loc["Y"]
+        diag = []
+        while x < board_size-1 and y < board_size-1:
+            x+=1
+            y+=1
+            diag.append((x, y))
+      
+        while x > 0 and y > 0:
+            x-=1
+            y-=1
+            diag.append((x, y))
+        
+        return diag
--- tests/test_player.py
+++ tests/test_player.py
@@ -11,8 +11,16 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(self.player.loc["X"], 3)
         self.assertEqual(self.player.loc["Y"], 0)
     
-    def test_get_actions(self):
-        legal_actions = self.player.actions(5)
-        self.assertTrue(all(elem in legal_actions for elem in [(3, 0), (3, 1), (3, 2), (1, 0), (2, 0)]))
+    def test_get_actions_at_perimiter(self):
+        legal_actions = self.player.actions_rows(5)
+        self.assertTrue(all(elem in legal_actions for elem in [(3, 0), (3, 1), (3, 2), (1, 0), (2, 0), (4, 1)]))
+    
+    def test_get_positive_negative_diag(self):
+        self.player.pos(3, 3)
+        legal_actions = self.player.actions_rows(5)
+        diags = [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)]
+        print(legal_actions)
+        self.assertTrue(all(elem in legal_actions for elem in diags))
+
 
       
