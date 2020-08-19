47d913e930c54aaef453e62b77ee0be578143963
Daniel Miller <daniel.miller@sainsburys.co.uk>
2020-08-19 08:49:10 +0100

    naive encoding done: uml game based encoding

--- player.py
+++ player.py
@@ -1,3 +1,5 @@
+import copy
+import time
 class Player:
     def __init__(self, symbol: str):
         self.symbol = symbol
@@ -10,13 +12,14 @@ class Player:
     def actions_rows(self, board_size: int) -> dict:
         actions = {}
         x, y = self.loc["X"], self.loc["Y"]
-        rows = [(x, i) for i in range(0, board_size)]
-        cols = [(i, y) for i in range(0, board_size)]
+        horizontal = [(x, i) for i in range(0, board_size)]
+        vertical = [(i, y) for i in range(0, board_size)]
         diags = self._diags(board_size)
-        return set(rows + cols + diags)
+        return set(horizontal + vertical + diags)
 
     def _diags(self, board_size: int) -> dict:
         x, y = self.loc["X"], self.loc["Y"]
+        x_pos, y_neg = copy.deepcopy(x), copy.deepcopy(y)
         diag = []
         while x < board_size-1 and y < board_size-1:
             x+=1
@@ -27,5 +30,14 @@ class Player:
             x-=1
             y-=1
             diag.append((x, y))
+
+        while x_pos > 0 and y_neg < board_size-1:
+            x_pos-=1
+            y_neg+=1
+            diag.append((x_pos, y_neg))
         
+        while x_pos < board_size-1 and y_neg > 0:
+            x_pos += 1
+            y_neg -=1
+            diag.append((x_pos, y_neg))
         return diag
--- tests/test_player.py
+++ tests/test_player.py
@@ -13,14 +13,19 @@ class TestPlayer(unittest.TestCase):
     
     def test_get_actions_at_perimiter(self):
         legal_actions = self.player.actions_rows(5)
-        self.assertTrue(all(elem in legal_actions for elem in [(3, 0), (3, 1), (3, 2), (1, 0), (2, 0), (4, 1)]))
+        self.assertTrue(all(elem in legal_actions for elem in [(3, 0), (3, 1), (3, 2), (3, 3)]))
     
     def test_get_positive_negative_diag(self):
         self.player.pos(3, 3)
         legal_actions = self.player.actions_rows(5)
-        diags = [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)]
-        print(legal_actions)
+        diags = [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (4, 2), (2, 4)]
         self.assertTrue(all(elem in legal_actions for elem in diags))
+    
+    def test_get_all_moves_from_max_point(self):
+        max_diags = [(0, 4), (4, 0), (4, 4), (0, 0)]
+        self.player.pos(2, 2)
+        legal_actions = self.player.actions_rows(5)
+        self.assertTrue(all(elem in legal_actions for elem in max_diags))
 
 
       
